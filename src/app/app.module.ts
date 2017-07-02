8de6f9ef58598db8a6b4ceb8d3ed31b31dab1698
Eric Murimi <emurinyo@gmail.com>
2017-07-02 13:46:24 +0300

    Setting up a wildcard route and redirecting routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list.component';
 import { HeroListComponent } from './hero-list.component';
+import {PageNotFoundComponent} from './not-found.component';
 
 const appRoutes: Routes = [
   { path: 'crisis-center', component: CrisisListComponent },
-  { path: 'heroes', component: HeroListComponent }
-]
+  { path: 'heroes', component: HeroListComponent },
+  { path: '', redirectTo: '/heroes', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     CrisisListComponent,
-    HeroListComponent
+    HeroListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/not-found.component.ts
@@ -0,0 +1,6 @@
+import {Component} from '@angular/core';
+@Component({
+  selector: 'not-found',
+  template: `<h2>Page not found</h2>`
+})
+export class PageNotFoundComponent {}
